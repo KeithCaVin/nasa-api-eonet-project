746e2ad7391530dd40c54ad2acb8d6c24618882b
KeithCaVin <54549002+KeithCaVin@users.noreply.github.com>
2021-05-22 23:58:23 +0400

    H1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Try
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To Tiyempu</h1>
     </div>
   );
 }
